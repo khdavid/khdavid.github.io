8dd68a5c7a5aec53e5db068276392f8736bad5b7
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-12-06 16:18:58 +0100

    stylistic changes

--- mandelbrot/glMandelbrot.js
+++ mandelbrot/glMandelbrot.js
@@ -17,106 +17,103 @@ var fragmentShaderCode = `
  
  
  
- function initGL(canvas) {
-     var gl
-     try {
-         gl = canvas.getContext("webgl");
-         gl.viewportWidth = canvas.width;
-         gl.viewportHeight = canvas.height;
-     } catch (e) {
-     }
-     if (!gl) {
-         alert("WebGL is not avaiable on your browser!");
-     }
-     return gl;
- }
+function initGL(canvas)
+{
+  var gl = canvas.getContext("webgl");
+  gl.viewportWidth = canvas.width;
+  gl.viewportHeight = canvas.height;
+
+  return gl;
+}
 
 
- function getShader(gl, shaderType, shaderCode) {
-     var shader = gl.createShader(shaderType);
-     gl.shaderSource(shader, shaderCode);
-     gl.compileShader(shader);
+function getShader(gl, shaderType, shaderCode) 
+{
+  var shader = gl.createShader(shaderType);
+  gl.shaderSource(shader, shaderCode);
+  gl.compileShader(shader);
 
-     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-         alert(gl.getShaderInfoLog(shader));
-         return null;
-     }
-
-     return shader;
- }
-
-
-  function initShaders(gl) {
-     var fragmentShader = getShader(gl, gl.FRAGMENT_SHADER, fragmentShaderCode);
-     var vertexShader = getShader(gl, gl.VERTEX_SHADER, vertexShaderCode);
-
-     shader_prog = gl.createProgram();
-     gl.attachShader(shader_prog, vertexShader);
-     gl.attachShader(shader_prog, fragmentShader);
-     gl.linkProgram(shader_prog);
-
-     if (!gl.getProgramParameter(shader_prog, gl.LINK_STATUS)) {
-         alert("Could not initialise shaders");
-     }
-
-     gl.useProgram(shader_prog);
-
-     shader_prog.positionLocation = gl.getAttribLocation(shader_prog, "Position");
-     gl.enableVertexAttribArray(shader_prog.positionLocation);
-
-     shader_prog.u_PerspLocation = gl.getUniformLocation(shader_prog, "u_Persp");
-     shader_prog.u_ModelViewLocation = gl.getUniformLocation(shader_prog, "u_ModelView");
-     
-     return shader_prog;
- }
-
-
-
- function initBuffers(gl) {
-
-     triangleVertexPositionBuffer = gl.createBuffer();
-     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-     var vertices = [
-          0.0,  1.0,  0.0,
-         -1.0, -1.0,  0.0,
-          1.0, -1.0,  0.0
-     ];
-     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-     triangleVertexPositionBuffer.itemSize = 3;
-     triangleVertexPositionBuffer.numItems = 3;
-     return triangleVertexPositionBuffer;
- }
-
-
- function drawScene(gl, triangleVertexPositionBuffer, shader_prog) {
-
-
-     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-     //Pass triangle position to vertex shader
-     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-     gl.vertexAttribPointer(shader_prog.positionLocation, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-
-     //Draw our lovely triangle
-     gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-
- }
-
- 
-   $( document ).ready(function() 
-  {
-     var canvas = document.getElementById("webgl_canvas");
-     var gl = initGL(canvas);
-     var shader_prog = initShaders(gl);
-     var triangleVertexPositionBuffer = initBuffers(gl);
-
-     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-     gl.enable(gl.DEPTH_TEST);
-
-     drawScene(gl, triangleVertexPositionBuffer, shader_prog);
-
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert(gl.getShaderInfoLog(shader));
+      return null;
   }
-  );
+
+  return shader;
+}
+
+
+function initShaders(gl) 
+{
+  var fragmentShader = getShader(gl, gl.FRAGMENT_SHADER, fragmentShaderCode);
+  var vertexShader = getShader(gl, gl.VERTEX_SHADER, vertexShaderCode);
+
+  shader_prog = gl.createProgram();
+  gl.attachShader(shader_prog, vertexShader);
+  gl.attachShader(shader_prog, fragmentShader);
+  gl.linkProgram(shader_prog);
+
+  if (!gl.getProgramParameter(shader_prog, gl.LINK_STATUS))
+  {
+    alert("Could not initialise shaders");
+  }
+
+  gl.useProgram(shader_prog);
+
+  shader_prog.positionLocation = gl.getAttribLocation(shader_prog, "Position");
+  gl.enableVertexAttribArray(shader_prog.positionLocation);
+
+  shader_prog.u_PerspLocation = gl.getUniformLocation(shader_prog, "u_Persp");
+  shader_prog.u_ModelViewLocation = gl.getUniformLocation(shader_prog, "u_ModelView");
+  
+  return shader_prog;
+}
+
+
+
+function initBuffers(gl) 
+{
+  triangleVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+  var vertices = [
+       0.0,  1.0,  0.0,
+      -1.0, -1.0,  0.0,
+       1.0, -1.0,  0.0
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  triangleVertexPositionBuffer.itemSize = 3;
+  triangleVertexPositionBuffer.numItems = 3;
+  return triangleVertexPositionBuffer;
+}
+
+
+function drawScene(gl, triangleVertexPositionBuffer, shader_prog)
+{
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  //Pass triangle position to vertex shader
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+  gl.vertexAttribPointer(shader_prog.positionLocation, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+
+  //Draw our lovely triangle
+  gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+
+}
+
+
+  $( document ).ready(function() 
+ {
+    var canvas = document.getElementById("webgl_canvas");
+    var gl = initGL(canvas);
+    var shader_prog = initShaders(gl);
+    var triangleVertexPositionBuffer = initBuffers(gl);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+
+    drawScene(gl, triangleVertexPositionBuffer, shader_prog);
+
+ }
+ );
 
